bc5d9b298e5fa9a50071b8f2d05cd3ffa289be87
wangshuaiqi <909770513@qq.com>
2017-09-20 17:21:47 +0800

    退出功能

--- public/js/common.js
+++ public/js/common.js
@@ -6,3 +6,28 @@
 	$('.navs ul').prev('a').on('click', function () {
 		$(this).next().slideToggle();
 	});
+
+	//实现退出功能
+	$("#logoutBtn").click(function() {
+		$.ajax({
+			url: '/api/logout',
+			type: 'post',
+			dataType: 'json',
+			success: function(data){
+				if(data.code==200){
+					console.log(data);
+					location.href='/main/login';
+				}
+			}
+		})
+	});
+	//检测用户登录
+	var flag = $.cookie('PHPSESSID');
+	if(!flag){
+		location.href='/main/login';
+	}
+	var loginInfo=$.cookie('loginInfo');
+	loginInfo=loginInfo && JSON.parse(loginInfo);
+	//设置头像
+	$(".aside .profile img").attr('src',loginInfo.tc_avatar);
+	$(".aside .profile h4").html(loginInfo.tc_name);

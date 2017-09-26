92f950ad25b2a4e3a81ae0099520f604d32d67de
wangshuaiqi <909770513@qq.com>
2017-09-26 20:41:09 +0800

    完成头像上传功能

--- public/js/config.js
+++ public/js/config.js
@@ -1,30 +1,34 @@
 require.config({
-	baseUrl:'/public/assets',
-	paths:{
-		jquery:'jquery/jquery',
-		cookie:'jquery-cookie/jquery.cookie',
-		bootstrap:'bootstrap/js/bootstrap.min',
-		template:'artTemplate/template-web',
-		validate:'validate/jquery-validate',
-		form:'jquery-form/jquery.form',
-		datepicker:'bootstrap-datepicker/js/bootstrap-datepicker',
-		language:'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
-		util:'../js/util',
-		teacherlist:'../js/teacherlist',
-		teacheradd:'../js/teacheradd',
-		common:'../js/common',
-		login:'../js/login',
-		settings:'../js/settings',
-	},
-	shim:{
-		bootstrap:{
-			deps:['jquery']
-		},
-		language:{
-			deps:['jquery','datepicker']
-		},
-		validate:{
-			deps:['jquery']
-		}
-	}
+    baseUrl: '/public/assets',
+    paths: {
+        jquery: 'jquery/jquery',
+        cookie: 'jquery-cookie/jquery.cookie',
+        bootstrap: 'bootstrap/js/bootstrap.min',
+        template: 'artTemplate/template-web',
+        validate: 'validate/jquery-validate',
+        form: 'jquery-form/jquery.form',
+        uploadify: 'uploadify/jquery.uploadify.min',
+        datepicker: 'bootstrap-datepicker/js/bootstrap-datepicker',
+        language: 'bootstrap-datepicker/locales/bootstrap-datepicker.zh-CN.min',
+        util: '../js/util',
+        teacherlist: '../js/teacherlist',
+        teacheradd: '../js/teacheradd',
+        common: '../js/common',
+        login: '../js/login',
+        settings: '../js/settings',
+    },
+    shim: {
+        bootstrap: {
+            deps: ['jquery']
+        },
+        language: {
+            deps: ['jquery', 'datepicker']
+        },
+        validate: {
+            deps: ['jquery']
+        },
+        uploadify: {
+            deps: ['jquery']
+        }
+    }
 });
--- public/js/settings.js
+++ public/js/settings.js
@@ -1,11 +1,25 @@
-define(['jquery', 'template'], function ($, template) {
+define(['jquery', 'template', 'uploadify'], function ($, template) {
     $.ajax({
         data: 'get',
         url: '/api/teacher/profile',
         datatype: 'json',
         success: function (data) {
             var html = template('settingsTpl', data.result);
-            $("#settingsInfo").html(html);
+            $("#settingsInfo").html(html)
+
+            $("#upfile").uploadify({
+                width:120,
+                height:120,
+                buttonText:"",
+                itemTemplate:'<span></span>',
+                swf: '/public/assets/uploadify/uploadify.swf',
+                uploader: '/api/uploader/avatar',
+                fileObjName: 'tc_avatar',
+                onUploadSuccess: function (a, b) {
+                    var obj = JSON.parse(b);
+                    $('.preview img').attr('src',obj.result.path);
+                }
+            })
         }
     })
 });

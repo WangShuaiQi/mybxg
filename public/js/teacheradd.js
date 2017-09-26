059d94109980f4c4d0e73f8eccb11d248f0cdb30
wangshuaiqi <909770513@qq.com>
2017-09-26 11:27:45 +0800

    完成讲师添加编辑功能

---
+++ public/js/teacheradd.js
@@ -0,0 +1,41 @@
+define(['jquery','template','util'],function($,template,util){
+    var tcId = util.qs("tc_id");
+    if(tcId){
+        //编辑讲师
+        $.ajax({
+            type:'get',
+            url:'/api/teacher/edit',
+            data:{tc_id:tcId},
+            typeof:'json',
+            success:function(data){
+                console.log(data);
+                data.result.operate='编辑讲师';
+                var html = template("teacherTpl",data.result);
+                $("#teacherInfo").html(html);
+                //处理表单提交
+                submitForm('/api/teacher/update');
+            }
+        })
+    }else{
+        //添加讲师
+        var html = template("teacherTpl",{operate:'添加讲师'});
+        $("#teacherInfo").html(html);
+        submitForm('/api/teacher/add');
+    }
+    //表单提交方法
+    function submitForm(url){
+        $("#teacherBtn").click(function(){
+            $.ajax({
+                type:'post',
+                url:url,
+                data:$("#teacherForm").serialize(),
+                datatype:'json',
+                success:function(data){
+                    if(data.code==200){
+                        location.href='/teacher/list';
+                    }
+                }
+            })
+        })
+    }
+});
